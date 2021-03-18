d475b88bb66583e1c2cc70486ac828f92b59ec17
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-03-18 11:20:18 +0100

    elso probalkozas

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,24 @@
+﻿var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(`${i} : ${faktoriális(i)}`);
+}
+
+function számítás() {
+    let n = document.getElementById("nTb").value;
+    let n2 = parseInt(n);
+    if (n2) {
+        let er = faktoriális(n);
+        document.getElementById("eredményDiv").innerText = er;
+    }
+    else {
+        document.getElementById("eredményDiv").innerText = "?";
+    }
+    
+}
