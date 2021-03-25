6d954cabba21944f016ff9fa6e78c29aa7459056
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-03-25 11:21:19 +0100

    kezdet

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,19 @@
+﻿window.onload = letöltés() && kérdésMegjelenítés(kérdés)
+var kérdések;
+function letöltés() {
+    fetch("/questions.json")
+        .then(response => response.json())
+        .then(d => letöltésBefejeződött(d)
+        );
+}
+function letöltésBefejeződött(data) {
+    console.log("Sikeres letöltés")
+    console.log(data)
+    kérdések = data;
+}
+
+function kérdésMegjelenítés(kérdés) {
+
+}
+function Next() { document.body.style.backgroundColor = "#ff0000"; }
+function Back() { document.body.style.backgroundColor = "#00ff00"; }
