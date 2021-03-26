d2115ae494775e9dfb6bca6201106f3e26c08272
Judit Bíborka Fabók <judit.fabok@stud.uni-corvinus.hu>
2021-03-26 11:13:06 +0100

    masodik

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -1,19 +1,67 @@
-﻿window.onload = letöltés() && kérdésMegjelenítés(kérdés)
+﻿window.onload = function () {
+
+    document.getElementById("back").onclick = function Vissza() {
+        kérdésSorszám--;
+        if (kérdésSorszám < 0) {
+            kérdésSorszám = kérdések.length - 1;
+        }
+        kérdésMegjelenítés(kérdésSorszám)
+    }
+
+    document.getElementById("next").onclick = function Előre() {
+        kérdésSorszám++;
+        if (kérdésSorszám == kérdések.length - 1) {
+            kérdésSorszám = 0;
+
+        }
+        kérdésMegjelenítés(kérdésSorszám);
+
+    }
+
+    letöltés();
+
+}
+
 var kérdések;
+var kérdésSorszám = 0;
+
 function letöltés() {
-    fetch("/questions.json")
+    fetch('questions.json')
         .then(response => response.json())
-        .then(d => letöltésBefejeződött(d)
+        .then(data => letöltésBefejeződött(data)
         );
 }
+
 function letöltésBefejeződött(data) {
     console.log("Sikeres letöltés")
     console.log(data)
     kérdések = data;
+    kérdésMegjelenítés(0);
 }
 
-function kérdésMegjelenítés(kérdés) {
+function kérdésMegjelenítés(kérdésSzáma) {
+    let kérdés_szöveg = document.getElementById("kérdés_szöveg");
+    let kép = document.getElementById("kép1");
+    let válasz1 = document.getElementById("válasz1");
+    let válasz2 = document.getElementById("válasz2");
+    let válasz3 = document.getElementById("válasz3");
 
+    kérdés_szöveg.innerHTML = kérdések[kérdésSzáma].questionText
+    kép.src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdésSzáma].image
+    válasz1.innerText = kérdések[kérdésSzáma].answer1
+    válasz2.innerText = kérdések[kérdésSzáma].answer2
+    válasz3.innerText = kérdések[kérdésSzáma].answer3
+
+    válasz1.classList.remove("jó", "rossz")
+    válasz2.classList.remove("jó", "rossz")
+    válasz3.classList.remove("jó", "rossz")
 }
-function Next() { document.body.style.backgroundColor = "#ff0000"; }
-function Back() { document.body.style.backgroundColor = "#00ff00"; }
+
+válasz = function (n) {
+    if (kérdések[kérdésSorszám].correctAnswer == n) {
+        document.getElementById("válasz" + n).classList.add("jó");
+    }
+    else {
+        document.getElementById("válasz" + n).classList.add("rossz");
+    }
+}
